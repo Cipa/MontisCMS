76e163e1c53a02e94bf8046983e60efeceae486d
Ciprian Badea <ciprian.badea@gmail.com>
2020-01-15 12:50:25 -0800

    installed UI and Vue

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/' . config('admin.path'), function () {
     return 'admin';
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
